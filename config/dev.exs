a1ed134cae54c1e5825153cfcb1d1845ca4eca2b
BryanJBryce <bryan.bryce@gmail.com>
2017-02-04 23:47:33 -0700

    Changes config for Swift Phoenix env

--- config/dev.exs
+++ config/dev.exs
@@ -5,6 +5,6 @@ config :logger, level: :error
 config :music_db, MusicDb.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "music_db_dev",
-  username: "postgres",
-  password: "postgres",
+  username: "bryan",
+  password: "",
   hostname: "localhost"
